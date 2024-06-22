2f29b350293deff00a95ed792f368157d15a0337
WOLF <blackwolf3466@gmail.com>
2024-06-22 06:29:55 +0300

    More features

--- electron/main.ts
+++ electron/main.ts
@@ -1,9 +1,11 @@
 import { app, BrowserWindow } from 'electron'
-import { createRequire } from 'node:module'
+/* import { createRequire } from 'node:module' */ // * Not used
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 
-const require = createRequire(import.meta.url)
+
+
+/* const require = createRequire(import.meta.url) */ // * Not used
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // The built directory structure
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,33 +1,51 @@
 import { useState } from 'react';
 import reactLogo from '../assets/react.svg';
-import viteLogo from '../assets/Vitejs-logo.svg.png';
+import viteLogo from '/electron-vite.animate.svg';
 import '../App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-        <div className="flex justify-center">
-          <a href="https://electron-vite.github.io" target="_blank" rel="noopener noreferrer">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button className='mb-4' onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-    </>
-  );
+    const [count, setCount] = useState(0);
+    const NOTIFICATION_TITLE = '🔔 Example notification'
+    const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
+    
+    return (
+        <>
+            <div className="flex justify-center">
+                <a href="https://electron-vite.github.io" target="_blank" rel="noopener noreferrer">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <div className='flex gap-4 mb-4 justify-center'>
+                    <button onClick={() => setCount((count) => count + 1)}>
+                        count is {count}
+                    </button>
+                    <button onClick={() => {
+                        if ('Notification' in window && Notification.permission === 'granted') {
+                            const notification = new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY });
+                          
+                            // Attach a click event handler to the notification
+                            notification.onclick = () => {
+                              console.log('Notification clicked!');
+                            };
+                          } else {
+                            console.error('Notifications are not supported or permission is not granted.');
+                          }
+                    }}>
+                        Test
+                    </button>
+                </div>
+                <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
 export default App;
